7aea0cd6391e37f6ac71c8356eb2e2513fff6dea
Cho In Seong <133129458+wind137944@users.noreply.github.com>
2023-05-23 20:34:15 +0900

    Add files via upload

---
+++ 5.22과제7/kmit_10724/src/com/kmit/www/seven.java
@@ -0,0 +1,12 @@
+package com.kmit.www;
+
+public class seven {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            for(int j=5;j>i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
