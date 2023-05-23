b7ef83c73fa802506014c783f989efee1876e597
Cho In Seong <133129458+wind137944@users.noreply.github.com>
2023-05-23 20:06:48 +0900

    Add files via upload

---
+++ 5.22과제3/kmit_10724/src/com/kmit/www/three.java
@@ -0,0 +1,12 @@
+package com.kmit.www;
+
+public class three {
+    public static void main (String[] args) {
+         char c = 'a';
+
+            do {
+                System.out.print(c);
+                  c++;
+              } while (c <= 'z');
+}
+}
