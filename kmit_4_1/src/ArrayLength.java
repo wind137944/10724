8562f865d64b6948e42abc6b7eb4d6e19ff687ba
Cho In Seong <133129458+wind137944@users.noreply.github.com>
2023-05-29 14:47:29 +0900

    5/29 과제[4-1]

---
+++ kmit_4_1/src/ArrayLength.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ArrayLength {
+    public static void main(String[] args) {
+        int intArray[]=new int[4];
+        int sum=0;
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.print(intArray.length+"개의 정수를 입력하세요\n>");
+        for (int i=0;i<intArray.length;i++)
+            intArray[i]=scanner.nextInt();
+        for (int i=0;i<intArray.length;i++)
+            sum+=intArray[i];
+
+        System.out.println("평균은 "+(double)sum/intArray.length);
+        scanner.close();
+    }
+
+}
